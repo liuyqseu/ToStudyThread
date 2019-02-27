25515edaecc8c6ef91b0013f00ee96c9c41a4330
liuyqseu <793586443@qq.com>
2019-02-27 19:20:40 +0800

    多线程相关测试类

---
+++ src/com/liu/thread/DaemonThreadFactory.java
@@ -0,0 +1,14 @@
+package com.liu.thread;
+
+import java.util.concurrent.ThreadFactory;
+
+public class DaemonThreadFactory implements ThreadFactory {
+
+	@Override
+	public Thread newThread(Runnable r) {
+		Thread thread = new Thread(r);
+		thread.setDaemon(true);
+		return thread;
+	}
+	
+}
---
+++ src/com/liu/thread/LiftOff.java
@@ -0,0 +1,33 @@
+package com.liu.thread;
+
+public class LiftOff implements Runnable {
+	
+	protected int countDown = 10;
+	private static int taskCount = 0;
+	private final int id = taskCount ++;
+	
+	public LiftOff() {}
+	
+	public LiftOff(int countDown) {
+		this.countDown = countDown;
+	}
+	
+	public String status() {
+		return "#" + id + "(" + (countDown > 0 ? countDown : "LiftOff!") + "). ";
+	}
+	
+	@Override
+	public void run() {
+		
+		while(countDown -- > 0) {
+			System.out.println(status());
+//			Thread.yield();
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
